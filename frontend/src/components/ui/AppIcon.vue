c305ab769175fd00bde0caabcda0722dd8d156b5
Lleonsnow <lord4714snow@gmail.com>
2026-03-23 22:14:17 +0300

    Собрать левую навигацию и основной layout страницы

---
+++ frontend/src/components/ui/AppIcon.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import { computed } from 'vue'
+import { Icon } from '@iconify/vue'
+
+type Props = {
+  icon: string
+  size?: number
+  color?: string
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  size: 24,
+})
+
+const iconProps = computed(() => ({
+  icon: props.icon,
+  width: props.size,
+  height: props.size,
+  color: props.color,
+}))
+</script>
+
+<template>
+  <Icon v-bind="iconProps" />
+</template>
+
